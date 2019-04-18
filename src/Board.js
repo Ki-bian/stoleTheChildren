c2d949bf4fc8ce0209f3f0e47e61fdd636249114
Kibian <kevcass01@yahoo.fr>
2019-04-18 22:18:47 +0200

    oeuf et bébé

---
+++ src/Board.js
@@ -0,0 +1,77 @@
+
+import React, { Component } from 'react';
+import './Game.css';
+import bebe from './assets/img/bebe2.gif'
+import bebe2 from './assets/img/bebe touché.gif'
+import Egg from './Egg';
+import EggsValue from './EggsValue';
+import Score from './composants/Score';
+
+class Board extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            score: 0,
+            pointer: 0,
+            pointer2: 0,
+            pointer3: 0,
+            imgs: [
+              bebe,
+              bebe2,
+            ],
+            imgs2: [
+                bebe,
+                bebe2,
+            ],
+            imgs3: [
+            bebe,
+            bebe2,
+            ]
+          };
+      
+          this.handleClick = this.handleClick.bind(this);
+          this.handleClick2 = this.handleClick2.bind(this);
+          this.handleClick3 = this.handleClick3.bind(this);
+        }
+      
+        handleClick() {
+          const { length } = this.state.imgs;
+          const { pointer } = this.state;
+          const newPointer =  pointer === length - 1 ? 0 : pointer + 1;
+          this.setState({ pointer: newPointer });
+        }
+
+        handleClick2() {
+            const { length } = this.state.imgs2;
+            const { pointer2 } = this.state;
+            const newPointer =  pointer2 === length - 1 ? 0 : pointer2 + 1;
+            this.setState({ pointer2: newPointer });
+        }
+
+        handleClick3() {
+            const { length } = this.state.imgs3;
+            const { pointer3 } = this.state;
+            const newPointer =  pointer3 === length - 1 ? 0 : pointer3 + 1;
+            this.setState({ pointer3: newPointer });
+        }
+        getScore = (newScore) => {
+            this.setState({ score: this.state.score + newScore });
+        }
+
+        render() {
+            const { pointer, pointer2, pointer3, imgs, imgs2, imgs3 } = this.state;
+
+          return (
+            <div className="board">
+                <img className="bebe" src={imgs[pointer]} onClick={this.handleClick} alt="bebe"/>
+                <img className="bebe2" src={imgs2[pointer2]} onClick={this.handleClick2} alt="bebe2"/>
+                <img className="bebe3" src={imgs3[pointer3]} onClick={this.handleClick3} alt="bebe3"/>
+                <Egg score={this.getScore} />
+                <Egg score={this.getScore} />
+                <Score newScore={this.state.score} />
+            </div>
+          );
+        }
+      
+      }
+export default Board;
--- src/Game.js
+++ src/Game.js
@@ -1,42 +1,31 @@
-
 import React, { Component } from 'react';
-import './Game.css';
 import axios from 'axios';
-import Egg from './Egg';
 import Board from './Board';
-import EggsValue from './EggsValue';
-import Score from './composants/Score';
+
 
 class Game extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            score: 0,
+            
         }
     }
-        componentWillMount() {
-            axios.get(`http://easteregg.wildcodeschool.fr/api/eggs/random`)
-                .then(res => {
-                    const egg = res.data;
-                    this.setState({ egg, });
-                })
-        }
-        getScore = (newScore) => {
-            this.setState({ score: this.state.score + newScore });
-        }
+    componentWillMount() {
+        axios.get(`http://easteregg.wildcodeschool.fr/api/eggs/random`)
+        .then(res => {
+                const egg = res.data;
+                this.setState({ egg, });
+            })
+    }
+    
 
-        render() {
-
-          return (
+    render() {
+        return (
             <div className="game">
-                <EggsValue />
-                <Egg score={this.getScore}/>
-                <div>{this.state.score}</div>
                 <Board />
-                <Score />
             </div>
-          );
-        }
-      
-      }
+        );
+    }
+}
+
 export default Game;
