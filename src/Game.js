9469cb8856b4f1986fa94bd02dc998b144e00c3a
eugenie <Vous@exemple.com>
2019-04-18 17:44:41 +0200

    test

---
+++ src/Game.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Egg from './Egg';
+
+
+
+class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            score: 0,
+          }
+    }
+    componentWillMount() {
+        axios.get(`http://easteregg.wildcodeschool.fr/api/eggs/random`)
+            .then(res => {
+                const egg = res.data;
+                this.setState({ egg, });
+            })
+    }
+    getScore = (newScore) => {
+        this.setState({ score : this.state.score + newScore });
+    }
+
+    render() { 
+        return ( 
+            <div className="game">
+                <Egg score={this.getScore}/>
+                <div>{this.state.score}</div>
+            </div>
+         );
+    }
+}
+ 
+export default Game;
