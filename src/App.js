00feacf904b2a4765b1f28abbb6386b2d444bb66
Kibian <kevcass01@yahoo.fr>
2019-04-18 21:28:09 +0200

    terrain et bébé

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Game from './Game.js'
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div><Game /></div>
     );
   }
 }
