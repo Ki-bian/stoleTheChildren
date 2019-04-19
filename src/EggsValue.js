61fe6862c207ed10377f90e38cb92c725c8b89a3
Marc-Gregoire Balme <marcbalme@yahoo.com>
2019-04-19 03:43:27 +0200

    mise en page Game

--- src/EggsValue.js
+++ src/EggsValue.js
@@ -3,42 +3,34 @@ import './EggsValue.css';
 
 const eggTab = [
     {
-        name: 'Junk',
         score: 1,
         image: "./junk.png",
     },
     {
-        name: 'Basic',
         score: 2,
         image: "./basic.png",
     },
     {
-        name: 'Fine',
         score: 3,
         image: "./fine.png",
     },
     {
-        name: 'Ascended',
         score: 4,
         image: "./ascended.png",
     },
     {
-        name: 'Exotic',
         score: 5,
         image: "./exotic.png",
     },
     {
-        name: 'Rare',
         score: 6,
         image: "./rare.png",
     },
     {
-        name: 'Masterwork',
         score: 7,
         image: "./masterwork.png",
     },
     {
-        name: 'Legendary',
         score: 8,
         image: "./legendary.png",
     }
@@ -54,7 +46,7 @@ class EggsValue extends Component {
                     eggTab.map((eggitem, index) => (
                         <div className="eggs">
                             <h3>{eggitem.name}</h3>
-                            <p>Points : {eggitem.score}</p>
+                            <p id='ptsnumb'>{eggitem.score} pts</p>
                             <img src={eggitem.image} alt={eggitem.name} />
                         </div>
                     ))
