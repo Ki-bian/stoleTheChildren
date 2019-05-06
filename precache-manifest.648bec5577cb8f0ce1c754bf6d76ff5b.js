8e0aab4a8a287e2539d41f68cbe3af1c34180bbe
Kibian <kevcass01@yahoo.fr>
2019-05-07 00:59:15 +0200

    Updates

---
+++ precache-manifest.648bec5577cb8f0ce1c754bf6d76ff5b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7b83b5ac0ae0e09faaa",
+    "url": "/stoleTheChildren/static/css/main.d632d989.chunk.css"
+  },
+  {
+    "revision": "b7b83b5ac0ae0e09faaa",
+    "url": "/stoleTheChildren/static/js/main.070fbb09.chunk.js"
+  },
+  {
+    "revision": "d48d297bb3571b275a18",
+    "url": "/stoleTheChildren/static/js/runtime~main.d7469f34.js"
+  },
+  {
+    "revision": "d929d6aa48e9dcd3cc12",
+    "url": "/stoleTheChildren/static/css/2.a11f7292.chunk.css"
+  },
+  {
+    "revision": "d929d6aa48e9dcd3cc12",
+    "url": "/stoleTheChildren/static/js/2.b2e17df6.chunk.js"
+  },
+  {
+    "revision": "c9efd6ce763c4bbfa243bac9498a3df1",
+    "url": "/stoleTheChildren/static/media/bebe2.c9efd6ce.gif"
+  },
+  {
+    "revision": "6d472197054a416990282cf9d3ddf49c",
+    "url": "/stoleTheChildren/static/media/bebe touché.6d472197.gif"
+  },
+  {
+    "revision": "21d25d0cb092e74a67db8c7aec9b5dc4",
+    "url": "/stoleTheChildren/static/media/background.21d25d0c.png"
+  },
+  {
+    "revision": "c6154be608bbd0f01f539908c56c253b",
+    "url": "/stoleTheChildren/index.html"
+  }
+];
